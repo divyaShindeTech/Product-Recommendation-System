d22d007c7fc637a45620b33864f19ab8668355b7
Divya Shinde <divyapshinde1@gmail.com>
2026-06-03 17:53:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ st.write("Get product recommendations")
 # Product selection
 product_name = st.selectbox(
     "Select Product",
-   df['productId'].unique()
+   df['productid'].unique()
 )
 # Recommendation button
 if st.button("Recommend"):
 
     recommendations = df[
-        df['productId'] != 'productId'
+        df['productid'] != 'productid'
     ].sample(5)
 
     st.subheader("Recommended Products")
 
-    for item in recommendations['productId']:
+    for item in recommendations['productid']:
         st.write(item)
 
-    for item in recommendations['productId']:
+    for item in recommendations['productid']:
         st.write(item)
